9933564afb75be2ad3e111f32806a4d3f3ae7b07
Göran Eriksson <goran.eriksson@axis.com>
2021-09-02 14:50:22 +0200

    Changes to yadm and other after upgrading to Debian 11

--- .config/fish/functions/axproxy.fish
+++ .config/fish/functions/axproxy.fish
@@ -2,7 +2,7 @@ function axproxy
 
     set SCOPE -U
     set PROXY "http://wwwproxy.se.axis.com:3128"
-    set NO_PROXY "localhost,127.0.0.1,localaddress,.localdomain.com,.se.axis.com,seconnect2.axis.com,connect2.axis.com"
+    set NO_PROXY "localhost,127.0.0.1,localaddress,.localdomain.com,.se.axis.com,seconnect2.axis.com,connect2.axis.com,vpncloud.axis.com,.gpcloudservice.com"
     set PROXY_VARS HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
     set HELP "Usage: axproxy enable|disable|status [SCOPE]
 
@@ -35,16 +35,19 @@ function axproxy
             set $SCOPE -x $PROXY_VAR $PROXY
         end
         set $SCOPE -x no_proxy $NO_PROXY
+        set $SCOPE -x NO_PROXY $NO_PROXY
     case disable
         for PROXY_VAR in $PROXY_VARS
             set $SCOPE -e $PROXY_VAR
         end
         set $SCOPE -e no_proxy
+        set $SCOPE -e NO_PROXY
     case status
         for PROXY_VAR in $PROXY_VARS
             set -S $PROXY_VAR
         end
         set -S no_proxy
+        set -S NO_PROXY
     case '*'
         echo $HELP
     end
