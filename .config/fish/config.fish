49b54a146a0340fd01e2158644dde51bb0858517
Göran Eriksson <goran.eriksson@axis.com>
2021-10-21 10:40:33 +0200

    Add alias to search wikipedia

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,6 +20,7 @@ set -gx PATH /sbin /usr/sbin /usr/local/sbin /home/gorane/.local/bin $PATH
 # Also needs `set -g default-terminal "screen-256color"`
 # in tmux.conf
 alias tmux "env TERM=screen-256color tmux"
+alias wiki "ddgr --np --gb !w $argv[1]"
 
 ### GIT PROMPT CONFIGURATION
 # See the file /usr/share/fish/functions/fish_git_prompt.fish
