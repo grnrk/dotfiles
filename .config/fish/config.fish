a8397055e27c1c478a046acbdb32d96587d87d99
Göran Eriksson <goran.eriksson@axis.com>
2022-01-22 21:57:27 +0100

    Add vimwiki aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,6 +23,8 @@ alias tmux "env TERM=screen-256color tmux"
 alias wiki "ddgr --np --gb !w $argv[1]"
 alias ddg "ddgr --np --gb !duckduckgo $argv[1]"
 alias vim "nvim"
+alias vw "vim -c VimwikiIndex"
+alias vwd "vim -c VimwikiDiaryIndex"
 
 ### GIT PROMPT CONFIGURATION
 # See the file /usr/share/fish/functions/fish_git_prompt.fish
