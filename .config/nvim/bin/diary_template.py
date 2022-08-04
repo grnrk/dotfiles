97584f8a8719e79d49c99934116be7eccdb59147
Göran Eriksson <goran.eriksson@axis.com>
2022-08-04 10:52:40 +0200

    diary template changes

--- .config/nvim/bin/diary_template.py
+++ .config/nvim/bin/diary_template.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-template = """## First things first!
+template = """## First things first! [P:1]
 
-  - [ ] Plan pomodoros [P:1]
+  - [ ] Check [Scratchpad](../Scratchpad)
+  - [ ] Plant tomatoes!
+      - [ ] Yesterdays overflow
+      - [ ] Calendar - Any recurring tasks or preparations needed?
+      - [ ] [TODO](../TODO) list
+      - [ ] [JIRA](https://jira.se.axis.com)
+
+## Planted tomatoes!
+
   - [ ] ...
-
-## Tomatoes of the day!
-
-  - [ ] ... [P:X]
-
-## Summary
-  - Canned tomatoes: X
-  - Crushed tomatoes:
 
 ## Notes"""
 print(template)
