a640a7356ce30daa2a38320698f9380e1c06a56f
Göran Eriksson <goran.eriksson@axis.com>
2024-07-02 14:10:45 +0200

    ocal fish function

---
+++ .config/fish/functions/ocal.fish
@@ -0,0 +1,9 @@
+function ocal
+    set ocaldir ~/Documents/script/o365
+    # Activate virtualenv
+    source $ocaldir/venv/bin/activate.fish
+    cd $ocaldir
+    python3 ocal.py 2> /dev/null
+    deactivate
+    cd -
+end
